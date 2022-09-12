9b5738474b4b75cd9674fd4563d91bcd276fff96
Feodora Bako <105170258+csakafeda@users.noreply.github.com>
2022-09-12 15:54:42 +0200

    Basic skeleton

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { useCharacters, useLocations } from "./api/useData";
+import Main from "./components/main";
+import { useFetch } from "./api/useFetch";
+import { useEffect } from "react";
 
 function App() {
+  const [char, setChar] = useState(false);
+
   const characters = useCharacters(1);
   const locations = useLocations(1);
 
+  useEffect(()=>{
+    if (characters === "Loading...") {
+      setChar(false);
+    } else {
+      setChar(true);
+    }
+  })
   console.log("Characters data: ");
   console.log(characters);
-  console.log("Locations data: ");
-  console.log(locations);
+  // console.log("Locations data: ");
+  // console.log(locations);
 
-  return <div className="App">Take a look at the console! (F12)</div>;
+  return (
+    <div className="App">
+      <div className="logo">----------Logo----------</div>
+      <button className="char-button">Characters</button>
+      <button className="location-button">Locations</button>
+      <div className="basic info">Hello! There is some infos for really big Rick and Morty fans</div>
+      
+      {/* {char ? (characters.results.map((char) => (<div className="cards" key={characters.result.name}>
+      {char.name}
+      </div>))) : (<div>{"valami"}</div>)} */}
+      
+      
+    </div>
+  );
 }
 
 export default App;
---
+++ react-and-morty/src/components/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useCharacters, useLocation } from "../api/useData";
+import { useFetch } from "../api/useFetch";
+
+
+export default function Main(characters){
+    // console.log(characters);
+    return(
+        <div className="infos">
+           {/* {characters ? characters.characters.results.map( (a)=>(<div>{a.name}</div>)): <div>{"valami"}</div> */}
+            {/* } */}
+            
+            
+
+        </div>
+    )
+}
