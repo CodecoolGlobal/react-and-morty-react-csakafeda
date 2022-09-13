f9e339207296934b4ecc04b1793e81985316fcab
Feodora Bako <105170258+csakafeda@users.noreply.github.com>
2022-09-13 13:24:26 +0200

    fix name display

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -11,30 +11,39 @@ function App() {
   const characters = useCharacters(1);
   const locations = useLocations(1);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (characters === "Loading...") {
       setChar(false);
     } else {
       setChar(true);
     }
-  })
+  });
   console.log("Characters data: ");
   console.log(characters);
+  console.log();
   // console.log("Locations data: ");
   // console.log(locations);
 
   return (
     <div className="App">
       <div className="logo">----------Logo----------</div>
-      <button className="char-button">Characters</button>
-      <button className="location-button">Locations</button>
-      <div className="basic info">Hello! There is some infos for really big Rick and Morty fans</div>
-      
-      {/* {char ? (characters.results.map((char) => (<div className="cards" key={characters.result.name}>
-      {char.name}
-      </div>))) : (<div>{"valami"}</div>)} */}
-      
-      
+      <div className="buttons">
+        <button className="char-button">Characters</button>
+        <button className="location-button">Locations</button>
+        <div className="basic info">
+          Hello! There is some infos for really big Rick and Morty fans
+        </div>
+
+        {char ? (
+          characters.results.map((char) => (
+            <div className="cards" key={char.name}>
+              {char.name}
+            </div>
+          ))
+        ) : (
+          <div>{"valami"}</div>
+        )}
+      </div>
     </div>
   );
 }
