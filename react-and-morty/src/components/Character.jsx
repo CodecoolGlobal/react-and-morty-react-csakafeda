1a76230a8535c612d3e68cc713d9a4f18d008da7
Feodora Bako <105170258+csakafeda@users.noreply.github.com>
2022-09-14 22:06:40 +0200

    fix last commit

--- react-and-morty/src/components/Character.jsx
+++ react-and-morty/src/components/Character.jsx
@@ -6,7 +6,8 @@ import Characterinfo from "./CharacterInfo";
 
 export default function CharacterListing({ charlist }) {
   const [characterList, setCharacterList] = useState(false);
-  const [search, setSearch] = useEffect(() => {
+  // const [search, setSearch] = useState("");
+  useEffect(() => {
     if (charlist.results === undefined) {
       setCharacterList(false);
     } else {
@@ -18,12 +19,12 @@ export default function CharacterListing({ charlist }) {
   if (characterList) {
     return (
       <div className="char-list">
-        <input
+        {/* <input
           type="text"
-          onChange={(text) => {
-            console.log(text.target.value);
-          }}
-        ></input>
+          // onChange={(text) => {
+          //   console.log(text.target.value);
+          // }}
+        ></input> */}
         {charactersArray.map((char) => (
           <div className="cards" key={char.id} id={char.id}>
             <Characterinfo charlist={char} />
